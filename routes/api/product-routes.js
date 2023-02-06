46a650c14a15b4741cee7cccf79a4230ffabdb79
Durgavino <mailtodurga93@gmail.com>
2023-02-06 16:36:49 -0400

    delete method on product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -89,8 +89,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const productdata=await Product.destroy({
+      where:{
+        id:req.params.id,
+      },
+    });
+    if(!productdata){
+      res.status(404).json({message:'No product found with that id!'});
+      return;
+    }
+    res.status(200).json(productdata);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
