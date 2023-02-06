e893f31e97475c6103b3b0bfe40f7b383f06fcf6
Durgavino <mailtodurga93@gmail.com>
2023-02-06 19:02:31 -0400

    add,delete,create the tag api

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -13,16 +13,40 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try{
+    const newtag=await Tag.create({
+      tag_id:req.body.tag_id,
+    });
+    res.status(200).json(newtag);
+  }
+  catch(err){
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tagdata=await Tag.destroy({
+      where:{id:req.params.id,
+      },
+    });
+    if(!tagdata)
+    {
+      res.status(404).json({message:'No tag found with that id!'});
+      return;
+    }
+    res.status(200).json(tagdata);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
